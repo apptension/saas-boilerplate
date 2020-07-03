6f71d811e7924674326f668591280a1c6f5003c0
Szymon Jedrasiak <sjedrasiak@apptension.com>
2020-07-03 12:42:57 +0200

    hello worker test fix

--- services/workers/userauth/handlers.py
+++ services/workers/userauth/handlers.py
@@ -9,6 +9,6 @@ def hello(event, context):
     logger.info(json.dumps(event, indent=2))
 
     with db_session() as session:
-        users = session.query(ua_models.User)
+        users = list(session.query(ua_models.User))
 
-    return {"users": json.dumps([dict(id=user.id, email=user.email, is_active=user.is_active) for user in users])}
+    return {"users": users}
