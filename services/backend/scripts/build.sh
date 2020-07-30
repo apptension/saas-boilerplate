c1a4af83da4d87b0899ae82864080b183c057504
Krzysztof Skoracki <krzysztof@skoracki.net>
2020-07-30 18:00:30 +0200

    Add check in ECR login for different CLI versions (#45)

--- services/backend/scripts/build.sh
+++ services/backend/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
 BACKEND_REPO_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$PROJECT_NAME-backend
@@ -9,8 +9,12 @@ TASK_IMAGE_URI_BACKEND=$BACKEND_REPO_URI:$VERSION
 
 docker build -t "$TASK_IMAGE_URI_BACKEND" .
 
-# shellcheck disable=SC2091
-$(aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
-#aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+if DOCKER_LOGIN=$(aws ecr get-login-password --region $AWS_DEFAULT_REGION 2> /dev/null); then
+  echo "$DOCKER_LOGIN" | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+else
+  echo "get-login-password not supported by the AWS CLI, trying get-login instead..."
+  # shellcheck disable=SC2091
+  $(aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
+fi
 
 docker push "$TASK_IMAGE_URI_BACKEND"
