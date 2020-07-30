7d20a28f4c3dec8cad5f5071bc91673d6d96899f
Patryk Ziemkowski <pziemkowski@apptension.com>
2020-07-30 13:09:06 +0200

    Fix tests

--- services/workers/userauth/tests/test_handlers.py
+++ services/workers/userauth/tests/test_handlers.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from .. import handlers
 
@@ -5,6 +6,6 @@ from .. import handlers
 @pytest.mark.usefixtures('db_session')
 def test_hello_returns_users(user_factory):
     users = user_factory.create_batch(3)
-    result = handlers.hello({}, {})
+    result = json.loads(handlers.hello({}, {}))
 
     assert len(users) == result['users']
