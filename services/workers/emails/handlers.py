fe65b7f0362882042325746e5d129ef31401fc25
Filip <filip.wolwowicz@gmail.com>
2020-08-03 11:46:51 +0200

    Unpack send-email event details (#49)
    * Unpack send-email event details
    
    * Use 'dict.get' built-in fallback
    
    Co-authored-by: Filip Wolwowicz <fwolwowicz@apptension.com>

--- services/workers/emails/handlers.py
+++ services/workers/emails/handlers.py
@@ -10,4 +10,4 @@ logger.setLevel(logging.INFO)
 def send_email(event, context):
     logger.info(json.dumps(event, indent=2))
 
-    sender.send_email(data=event)
+    sender.send_email(data=event.get("detail", event))
