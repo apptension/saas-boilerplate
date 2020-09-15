d27468f3172b5fdfba964db1df4c0744737b12e8
Szymon Jedrasiak <sjedrasiak@apptension.com>
2020-09-15 08:47:39 +0200

    typos in comments fix

--- services/backend/config/asgi.py
+++ services/backend/config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for config project.
+ASGI config for project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- services/backend/config/wsgi.py
+++ services/backend/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
