a2f16fba9307683cef5674e7eee060cbdb72da84
Patryk Ziemkowski <patryk.ziem@gmail.com>
2021-03-22 11:23:19 +0000

    SB-248 BE update subscription default payment method
    * SB-248 BE update subscription default payment method
    
    Approved-by: Michał Przyszczypkowski

--- services/backend/apps/finances/webhooks.py
+++ services/backend/apps/finances/webhooks.py
@@ -40,3 +40,24 @@ def activate_free_plan_on_subscription_deletion(event):
     customer = djstripe_models.Customer.objects.get(id=obj['customer'])
     free_plan_price = djstripe_models.Price.objects.get_by_plan(constants.FREE_PLAN)
     customer.subscribe(price=free_plan_price)
+
+
+@webhooks.handler('payment_method.attached')
+def update_subscription_default_payment_method(event):
+    """
+    Remove this webhook if you don't want the newest payment method
+    to be a default one for the subscription.
+    The best alternative approach would most likely be to create a custom API
+    endpoint that sets a default payment method on demand called right after
+    the web app succeeds setup intent confirmation.
+
+    :param event:
+    :return:
+    """
+
+    obj = event.data['object']
+    customer: djstripe_models.Customer = djstripe_models.Customer.objects.get(id=obj['customer'])
+    if not customer.subscription:
+        return
+
+    subscriptions.update(customer.subscription, default_payment_method=obj['id'])
