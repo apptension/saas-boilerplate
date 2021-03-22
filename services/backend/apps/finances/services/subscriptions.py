542c008efedbd6ca6e5f9bc5f3078b715f24d4c8
Patryk Ziemkowski <patryk.ziem@gmail.com>
2021-03-22 10:21:36 +0000

    SB-265 Downgrade subscription to free plan on payment fail
    * SB-265 Downgrade subscription to free plan on payment fail

--- services/backend/apps/finances/services/subscriptions.py
+++ services/backend/apps/finances/services/subscriptions.py
@@ -3,14 +3,24 @@ from djstripe import models as djstripe_models
 from .. import models, constants
 
 
-def update(instance, **kwargs):
+def update(instance: djstripe_models.Subscription, **kwargs):
+    """
+    This method replaced the Subscription.update method.
+
+    Built in Subscription.update method does not update related SubscriptionItems even though the data
+    returned from stripe has all the info needed.
+
+    :param instance: Subscription
+    :param kwargs:
+    :return:
+    """
     stripe_subscription = instance._api_update(**kwargs)
     subscription = djstripe_models.Subscription.sync_from_stripe_data(stripe_subscription)
     djstripe_models.SubscriptionItem.sync_from_stripe_data(stripe_subscription.get("items").data[0])
     return subscription
 
 
-def update_to_free_plan(instance, **kwargs):
+def update_to_free_plan(instance: djstripe_models.Subscription, **kwargs):
     free_plan_price = models.Price.objects.get_by_plan(constants.FREE_PLAN)
     subscription_item = instance.items.first()
     return update(instance, items=[{'id': subscription_item.id, 'price': free_plan_price.id}], **kwargs)
--- services/backend/apps/finances/webhooks.py
+++ services/backend/apps/finances/webhooks.py
@@ -1,15 +1,41 @@
 import logging
 
-from djstripe import models as djstripe_models
+from djstripe import models as djstripe_models, enums as djstripe_enums
 from djstripe import webhooks
 
 from . import constants
+from .services import subscriptions
 
 logger = logging.getLogger(__name__)
 
 
+@webhooks.handler('customer.subscription.updated')
+def downgrade_to_free_plan_for_past_due_subscription(event):
+    """
+    Subscription is has a past_due status after a recurring charge fails.
+
+    :param event:
+    :return:
+    """
+
+    obj = event.data['object']
+    if obj['status'] != djstripe_enums.SubscriptionStatus.past_due:
+        return
+
+    subscription = djstripe_models.Subscription.objects.get(id=obj['id'])
+    subscriptions.update_to_free_plan(subscription)
+
+
 @webhooks.handler('customer.subscription.deleted')
 def activate_free_plan_on_subscription_deletion(event):
+    """
+    It is not possible to reactivate a canceled subscription with a different plan so we
+    create a new subscription on a free plan
+
+    :param event:
+    :return:
+    """
+
     obj = event.data['object']
     customer = djstripe_models.Customer.objects.get(id=obj['customer'])
     free_plan_price = djstripe_models.Price.objects.get_by_plan(constants.FREE_PLAN)
