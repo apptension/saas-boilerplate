92a850ba8df844e28c66c45f82227e70e55f833a
Bartosz Koniński <bkoninski@apptension.com>
2021-06-15 11:37:26 +0000

    [SB-452] Using gevent as a worker_class for gunicorn.
    Approved-by: Krystian Hanek

--- services/backend/gunicorn.py
+++ services/backend/gunicorn.py
@@ -3,7 +3,7 @@ from multiprocessing import cpu_count
 
 
 def max_workers():
-    return cpu_count()
+    return cpu_count() * 2 + 1
 
 
 bind = "0.0.0.0:80"
@@ -11,3 +11,4 @@ max_requests = 1000
 accesslog = "-"
 errorlog = "-"
 workers = max_workers()
+worker_class = "gevent"
