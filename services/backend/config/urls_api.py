de26d53b4b12b46585d3d84a863ea0d7f1e2b710
Igor Bińkowski <ibinkowski@apptension.com>
2022-09-01 12:09:31 +0000

    feat: Allow HTTPS in API docs

--- services/backend/config/urls_api.py
+++ services/backend/config/urls_api.py
@@ -1,6 +1,7 @@
 from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from drf_yasg.generators import OpenAPISchemaGenerator
 from rest_framework import permissions
 from django.conf import settings
 
@@ -8,7 +9,20 @@ from common.graphql.views import DRFAuthenticatedGraphQLView
 
 api_info = openapi.Info(title="Documentation", default_version="v1")
 
-schema_view = get_schema_view(api_info, public=True, permission_classes=(permissions.AllowAny,))
+
+class HttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
+
+
+schema_view = get_schema_view(
+    api_info,
+    public=True,
+    generator_class=HttpAndHttpsSchemaGenerator,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
