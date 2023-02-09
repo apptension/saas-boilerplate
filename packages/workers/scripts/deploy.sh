f0d91967209a0f835722a5e8e02a52912e999987
Przemysław Michałek <pmichalek@apptension.com>
2023-02-09 15:43:49 +0000

    fix: SB-812 Include __pypackages__ in serverless workers deployment
    * fix: SB-812 Run pdm sync before serverless deployment
    
    
    Approved-by: Michał Kleszcz
    Approved-by: Patryk Ziemkowski

--- packages/workers/scripts/deploy.sh
+++ packages/workers/scripts/deploy.sh
@@ -2,5 +2,6 @@
 
 cd /app/packages/workers/ || exit
 
+pdm sync --prod # Sync command before deployment is needed to prevent deploying dev dependencies
 node_modules/.bin/sls --version
 node_modules/.bin/sls deploy --stage "${ENV_STAGE}"
