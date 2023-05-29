fb6a3b44454ea3ed61f7803ecf36450c079b13d2
Michał Kleszcz <mkleszcz@apptension.com>
2023-05-29 11:21:42 +0000

    fix: Invalid .versionrc.js file

--- .versionrc.js
+++ .versionrc.js
@@ -1,21 +1,75 @@
 module.exports = {
   bumpFiles: [
     {
-      filename: "./services/webapp/package.json",
-      type: "json",
+      filename: './packages/webapp/package.json',
+      type: 'json',
     },
     {
-      filename: "./services/workers/package.json",
-      type: "json",
+      filename: './packages/webapp-libs/webapp-api-client/package.json',
+      type: 'json',
     },
     {
-      filename: "./services/docs/package.json",
-      type: "json",
+      filename: './packages/webapp-libs/webapp-contentful/package.json',
+      type: 'json',
+    },
+
+    {
+      filename: './packages/webapp-libs/webapp-core/package.json',
+      type: 'json',
     },
     {
-      filename: "./services/contentful/package.json",
-      type: "json",
+      filename: './packages/webapp-libs/webapp-crud-demo/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/webapp-libs/webapp-documents/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/webapp-libs/webapp-emails/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/webapp-libs/webapp-finances/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/webapp-libs/webapp-generative-ai/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/webapp-libs/webapp-notifications/package.json',
+      type: 'json',
+    },
+
+    {
+      filename: './packages/workers/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/contentful/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/docs/core.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/docs/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/local-ws-server/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/status-dashboard/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/tools/package.json',
+      type: 'json',
     },
   ],
-  tagPrefix: "",
+  tagPrefix: '',
 };
