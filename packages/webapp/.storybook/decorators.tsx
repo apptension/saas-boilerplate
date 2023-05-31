5b9141975fa83b46619e98bba3955e0780c7a705
Michal Kleszcz <mkleszcz@apptension.com>
2023-05-31 10:46:08 +0200

    fix: #174 Fix storybooks

--- packages/webapp/.storybook/decorators.tsx
+++ packages/webapp/.storybook/decorators.tsx
@@ -1,9 +1,11 @@
 import { DEFAULT_LOCALE, translationMessages } from '@sb/webapp-core/config/i18n';
 import { ResponsiveThemeProvider } from '@sb/webapp-core/providers';
-import { global as globalTheme, initializeFontFace } from '@sb/webapp-core/theme';
+import { global as globalTheme } from '@sb/webapp-core/theme';
 import { StoryFn } from '@storybook/react';
 import { useEffect } from 'react';
 import { IntlProvider } from 'react-intl';
+
+import { initializeFontFace } from '../src/app/utils/initializeFontFace';
 
 export const withTheme = () => (StoryComponent: StoryFn) =>
   (
--- packages/webapp/.storybook/preview.ts
+++ packages/webapp/.storybook/preview.ts
@@ -1,6 +1,8 @@
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
 import { Preview } from '@storybook/react';
 import * as jest from 'jest-mock';
 
+import '../src/styles.css';
 import { withFontFace, withIntl, withTheme } from './decorators';
 
 //@ts-ignore
@@ -8,7 +10,19 @@ window.jest = jest;
 
 const preview: Preview = {
   parameters: { layout: 'fullscreen' },
-  decorators: [withIntl, withTheme(), withFontFace],
+  decorators: [
+    withIntl,
+    withTheme(),
+    withFontFace,
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-mode',
+    }),
+  ],
 };
 
 export default preview;
