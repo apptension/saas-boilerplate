3b168da1cc15763dd60907271c0bd8b3495f6266
bielsky0 <82337968+bielsky0@users.noreply.github.com>
2023-06-05 17:01:41 +0200

    feat:#174 Migrate to shadcn ui (#183)
    feat: #174 Migrate input component to Radix/shadcn ui

--- packages/webapp-libs/webapp-core/src/components/forms/input/input.component.tsx
+++ packages/webapp-libs/webapp-core/src/components/forms/input/input.component.tsx
@@ -1,27 +1,47 @@
-import { forwardRef, ReactNode, InputHTMLAttributes } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Container, Field, Message, Label, LabelText } from './input.styles';
-import { InputTheme } from './input.types';
+import { InputHTMLAttributes, ReactNode, forwardRef } from 'react';
+
+import { cn } from '../../../lib/utils';
 
 export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
   label?: ReactNode;
 };
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, error, label, required, ...inputProps }: InputProps, ref) => {
-    const theme: InputTheme = { invalid: !!error, required };
-
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, error, label, required, type, ...props }: InputProps, ref) => {
     return (
-      <ThemeProvider theme={theme}>
-        <Container className={className}>
-          <Label>
-            <Field {...inputProps} ref={ref} />
-            {label && <LabelText>{label}</LabelText>}
-          </Label>
-          <Message>{error}</Message>
-        </Container>
-      </ThemeProvider>
+      <div className={cn(`relative w-full max-w-xs`, className)}>
+        <label className="flex flex-col items-start">
+          <input
+            type={type}
+            className={cn(
+              `h-10 w-full border-input transition-all duration-200 ease-in rounded-md border border-input bg-transparent 
+              px-3 py-2 text-primary text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm 
+              file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 
+              focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
+              {
+                'border-red-500': !!error,
+              }
+            )}
+            ref={ref}
+            {...props}
+          />
+          {label && (
+            <p
+              className={cn(`text-xs mb-1 order-first`, {
+                'text-red-500': !!error,
+              })}
+            >
+              {label}
+            </p>
+          )}
+        </label>
+        <p className="leading-3 text-xs absolute m-0 top-[calc(100%+4px)] text-red-500">{error}</p>
+      </div>
     );
   }
 );
+
+Input.displayName = 'Input';
+
+export { Input };
