eb1ed52dae0761a482b60a8c21e78b886bfe7751
Jakub Bielski <jbielsky00@gmail.com>
2023-06-05 13:57:58 +0200

    feat:#174 Add testid to modal

--- packages/webapp-libs/webapp-core/src/components/modal/modal.component.tsx
+++ packages/webapp-libs/webapp-core/src/components/modal/modal.component.tsx
@@ -30,10 +30,16 @@ const DialogOverlay = forwardRef<
 ));
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
+type DialogContentProps = React.ForwardRefExoticComponent<
+  DialogPrimitive.DialogContentProps &
+    React.RefAttributes<HTMLDivElement> & {
+      testId?: string;
+    }
+>;
 const DialogContent = forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<DialogContentProps>
+>(({ className, children, testId = 'modal-icon-container', ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -46,7 +52,7 @@ const DialogContent = forwardRef<
     >
       {children}
       <DialogPrimitive.Close
-        data-testid="modal-icon-container"
+        data-testid={testId}
         className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
       >
         <X className="h-4 w-4" />
