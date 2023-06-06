fceed2453a9c55007a604ffa1af47d3d69b37d3e
Jakub Bielski <jbielsky00@gmail.com>
2023-06-06 15:16:54 +0200

    feat:#174 Add simple form test

---
+++ packages/webapp-libs/webapp-core/src/components/forms/form/__tests__/form.component.spec.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react';
+import { useForm } from 'react-hook-form';
+
+import { render } from '../../../../tests/utils/rendering';
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '../form.component';
+
+describe('Form: component', () => {
+  type MockedForm = {
+    label: string;
+  };
+
+  const Component = () => {
+    const form = useForm<MockedForm>();
+
+    return (
+      <Form {...form}>
+        <form>
+          <FormField
+            control={form.control}
+            name="label"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Label</FormLabel>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    );
+  };
+
+  it('Should render form with "Label" label', async () => {
+    render(<Component />);
+
+    expect(await screen.findByText('Label')).toBeInTheDocument();
+  });
+});
