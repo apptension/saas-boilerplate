930766e07e3f0cd6c1b08130221d9154f539dd4e
Jakub Bielski <jbielsky00@gmail.com>
2023-06-07 14:44:15 +0200

    feat:#174 Split dialog components

---
+++ packages/webapp-libs/webapp-core/src/components/dialog/dialogDescription/index.ts
@@ -0,0 +1,1 @@
+export * from './dialogDescription.component';
---
+++ packages/webapp-libs/webapp-core/src/components/dialog/dialogTrigger/dialogTrigger.component.tsx
@@ -0,0 +1,5 @@
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+
+const DialogTrigger = DialogPrimitive.Trigger;
+
+export { DialogTrigger };
