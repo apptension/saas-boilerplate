dfdb6c4a68faa6d73c2fe2d4bb2a76c2581cb48e
Jakub Bielski <jbielsky00@gmail.com>
2023-06-10 12:38:39 +0200

    feat:#174 Add tests

--- packages/webapp-libs/webapp-core/src/components/popover/__tests__/popover.component.spec.tsx
+++ packages/webapp-libs/webapp-core/src/components/popover/__tests__/popover.component.spec.tsx
@@ -1,11 +1,12 @@
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import { Popover, PopoverContent, PopoverTrigger } from '../';
 import { render } from '../../../tests/utils/rendering';
 
-describe('Form: component', () => {
+describe('Popover: component', () => {
   const Component = (props: React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Root>) => (
     <Popover {...props}>
       <PopoverTrigger>Trigger</PopoverTrigger>
@@ -16,6 +17,14 @@ describe('Form: component', () => {
   it('Should be visible by default', async () => {
     render(<Component defaultOpen={true} />);
 
-    expect(await screen.findByText('Trigger')).toBeInTheDocument();
+    expect(await screen.findByText('Content')).toBeInTheDocument();
+  });
+
+  it('Should be visible after click', async () => {
+    render(<Component defaultOpen={false} />);
+
+    await userEvent.click(await screen.findByText('Trigger'));
+
+    expect(await screen.findByText('Content')).toBeInTheDocument();
   });
 });
