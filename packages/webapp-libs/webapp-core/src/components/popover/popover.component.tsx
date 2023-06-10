67a257d1cee81adb5bb825d1defcfbc1b39973f0
Jakub Bielski <jbielsky00@gmail.com>
2023-06-10 10:18:26 +0200

    feat:#174 Add popover component

---
+++ packages/webapp-libs/webapp-core/src/components/popover/popover.component.tsx
@@ -0,0 +1,5 @@
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+
+const Popover = PopoverPrimitive.Root;
+
+export { Popover };
---
+++ packages/webapp-libs/webapp-core/src/components/popover/popoverTrigger/popoverTrigger.component.tsx
@@ -0,0 +1,5 @@
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+
+const PopoverTrigger = PopoverPrimitive.Trigger;
+
+export { PopoverTrigger };
