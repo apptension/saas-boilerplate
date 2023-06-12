21985506f55c84d2f3906f78c97dbb6d808d462f
Jakub Bielski <jbielsky00@gmail.com>
2023-06-12 09:42:12 +0200

    feat:#174 Adjust imports

--- packages/webapp-libs/webapp-core/src/components/avatar/avatar.component.tsx
+++ packages/webapp-libs/webapp-core/src/components/avatar/avatar.component.tsx
@@ -15,24 +15,4 @@ const Avatar = React.forwardRef<
 ));
 Avatar.displayName = AvatarPrimitive.Root.displayName;
 
-const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Image>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image ref={ref} className={cn('aspect-square h-full w-full', className)} {...props} />
-));
-AvatarImage.displayName = AvatarPrimitive.Image.displayName;
-
-const AvatarFallback = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Fallback>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Fallback
-    ref={ref}
-    className={cn('flex h-full w-full items-center justify-center rounded-full bg-muted', className)}
-    {...props}
-  />
-));
-AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
-
-export { Avatar, AvatarImage, AvatarFallback };
+export { Avatar };
--- packages/webapp-libs/webapp-core/src/components/avatar/index.ts
+++ packages/webapp-libs/webapp-core/src/components/avatar/index.ts
@@ -1,1 +1,3 @@
 export * from './avatar.component';
+export * from './avatarFallback';
+export * from './avatarImage';
