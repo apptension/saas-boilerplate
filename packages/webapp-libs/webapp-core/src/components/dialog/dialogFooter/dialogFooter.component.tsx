94a7fe658e6fd96c44bf86694976df07bed76dc8
bielsky0 <82337968+bielsky0@users.noreply.github.com>
2023-06-12 10:41:44 +0200

    Feature/radix UI modal (#187)
    * feat:#174 Install radix ui dialog
    
    * feat:#174 fix tests
    
    * feat:#174 Adjust tests
    
    * feat:#174 Adjust tests
    
    * feat:#174 Add testid to modal
    
    * feat:#174 Add testid to modal
    
    * chore: Remove unused styles
    
    * chore: Update pnpm-lock.yaml
    
    * feat:#174 Rename modal to dialog
    
    * feat:#174 Fix stories
    
    * feat:#174 Split dialog components
    
    * feat:#174 Fix imports
    
    * feat:#174 Fix space typo
    
    ---------
    
    Co-authored-by: Michal Kleszcz <mkleszcz@apptension.com>

---
+++ packages/webapp-libs/webapp-core/src/components/dialog/dialogFooter/dialogFooter.component.tsx
@@ -0,0 +1,8 @@
+import { cn } from '../../../lib/utils';
+
+const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
+);
+DialogFooter.displayName = 'DialogFooter';
+
+export { DialogFooter };
---
+++ packages/webapp-libs/webapp-core/src/components/dialog/dialogPortal/index.ts
@@ -0,0 +1,1 @@
+export * from './dialogPortal.component';
