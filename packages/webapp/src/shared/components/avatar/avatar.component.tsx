b50bffe89d25bc0ce7cac1f776e5b503ce32eb41
Jakub Bielski <jbielsky00@gmail.com>
2023-06-13 11:52:21 +0200

    feat:#174 Clean up

--- packages/webapp/src/shared/components/avatar/avatar.component.tsx
+++ packages/webapp/src/shared/components/avatar/avatar.component.tsx
@@ -10,18 +10,15 @@ export type AvatarProps = HTMLAttributes<HTMLSpanElement> & {
   size?: number;
 };
 
-export const Avatar = ({ size, ...props }: AvatarProps) => {
+export const Avatar = ({ size = 40, ...props }: AvatarProps) => {
   const intl = useIntl();
   const { currentUser } = useAuth();
 
   const avatar = currentUser?.avatar;
 
   return (
-    <AvatarContainer {...props} className={`w-[${size ? size : 40}px] h-[${size ? size : 40}px]`}>
+    <AvatarContainer {...props} className={`w-[${size}px] h-[${size}px]`}>
       <AvatarImage
-        onLoadingStatusChange={(status) => {
-          console.log(status, 'statusstatus');
-        }}
         src={isNil(avatar) ? undefined : avatar}
         alt={intl.formatMessage({ defaultMessage: 'user avatar', id: 'Avatar / Image alt' })}
       />
@@ -31,5 +28,3 @@ export const Avatar = ({ size, ...props }: AvatarProps) => {
     </AvatarContainer>
   );
 };
-
-// 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/315.jpg'
