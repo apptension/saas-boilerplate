a7ac833cdcaf1de41d55bb3079be70032cad4e0a
Kacper Adler <kadler@apptension.com>
2023-06-15 12:03:26 +0200

    feat: Add typography

---
+++ packages/webapp-libs/webapp-core/src/components/typography/typography.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta } from '@storybook/react';
+import { HtmlHTMLAttributes } from 'react';
+
+import { H1, H2, H3, H4, Paragraph, ParagraphBold, Small } from './typography';
+
+const Container = ({ children }: HtmlHTMLAttributes<HTMLDivElement>) => (
+  <div className="flex flex-row gap-4">{children}</div>
+);
+
+const meta: Meta<typeof Container> = {
+  title: 'Core/Typography',
+  component: Container,
+};
+
+export default meta;
+
+export const Primary = {
+  render: () => (
+    <Container>
+      <H1>Heading 1</H1>
+      <H2>Heading 1</H2>
+      <H3>Heading 1</H3>
+      <H4>Heading 1</H4>
+      <Paragraph>Paragraph</Paragraph>
+      <ParagraphBold>Paragraph bold</ParagraphBold>
+      <Small>Small</Small>
+    </Container>
+  ),
+};
---
+++ packages/webapp-libs/webapp-core/src/components/typography/typography.tsx
@@ -0,0 +1,65 @@
+import { HtmlHTMLAttributes, forwardRef } from 'react';
+
+import { cn } from '../../lib/utils';
+
+export type HeadingProps = HtmlHTMLAttributes<HTMLHeadElement>;
+
+export const H1 = forwardRef<HTMLHeadElement, HeadingProps>(({ children, className, ...props }) => {
+  return (
+    <h1 {...props} className={cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl', className)}>
+      {children}
+    </h1>
+  );
+});
+
+export const H2 = forwardRef<HTMLHeadElement, HeadingProps>(({ children, className, ...props }) => {
+  return (
+    <h2 {...props} className={cn('tracking-tight" scroll-m-20 text-3xl font-semibold', className)}>
+      {children}
+    </h2>
+  );
+});
+
+export const H3 = forwardRef<HTMLHeadElement, HeadingProps>(({ children, className, ...props }) => {
+  return (
+    <h3 {...props} className={cn('scroll-m-20 text-2xl font-semibold tracking-tight', className)}>
+      {children}
+    </h3>
+  );
+});
+
+export const H4 = forwardRef<HTMLHeadElement, HeadingProps>(({ children, className, ...props }) => {
+  return (
+    <h4 {...props} className={cn('scroll-m-20 text-xl font-semibold tracking-tight', className)}>
+      {children}
+    </h4>
+  );
+});
+
+export type ParagraphProps = HtmlHTMLAttributes<HTMLParagraphElement>;
+
+export const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(({ children, className, ...props }) => {
+  return (
+    <p {...props} className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}>
+      {children}
+    </p>
+  );
+});
+
+export const ParagraphBold = forwardRef<HTMLParagraphElement, ParagraphProps>(({ children, className, ...props }) => {
+  return (
+    <p {...props} className={cn('font-semibold leading-7 [&:not(:first-child)]:mt-6', className)}>
+      {children}
+    </p>
+  );
+});
+
+export type SmallProps = HtmlHTMLAttributes<HTMLElement>;
+
+export const Small = forwardRef<HTMLElement, SmallProps>(({ children, className, ...props }) => {
+  return (
+    <small {...props} className={cn('text-sm font-medium leading-none', className)}>
+      {children}
+    </small>
+  );
+});
