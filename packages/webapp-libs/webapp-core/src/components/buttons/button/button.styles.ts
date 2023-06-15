b0dbeedbff616046c990e5b40ad5ff7aaf0da6cd
Jakub Bielski <jbielsky00@gmail.com>
2023-06-15 13:59:22 +0200

    feat:#174 Use renderIcon in button

--- packages/webapp-libs/webapp-core/src/components/buttons/button/button.styles.ts
+++ packages/webapp-libs/webapp-core/src/components/buttons/button/button.styles.ts
@@ -1,12 +1,4 @@
 import { cva } from 'class-variance-authority';
-import styled from 'styled-components';
-
-export const Icon = styled.span`
-  margin-right: 4px;
-  font-size: 0;
-  display: inline-flex;
-  align-items: center;
-`;
 
 export const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
