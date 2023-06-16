ddf27d0473d468d7b11cc1f36beab972c09590b5
Kacper Adler <kadler@apptension.com>
2023-06-16 08:56:41 +0200

    feat: Create admin page example

---
+++ packages/webapp/src/routes/admin/admin.component.tsx
@@ -0,0 +1,30 @@
+import shieldIcon from '@iconify-icons/ion/shield-checkmark-outline';
+import { Card } from '@sb/webapp-core/components/cards';
+import { Icon } from '@sb/webapp-core/components/icons';
+import { H2, H4, Small } from '@sb/webapp-core/components/typography';
+import { FormattedMessage } from 'react-intl';
+
+export const Admin = () => {
+  return (
+    <div className="px-4">
+      <div className="w-full text-center">
+        <H2>
+          <FormattedMessage defaultMessage="This page is only visible for admins" id="Admin / Heading" />
+        </H2>
+      </div>
+      <div className="mt-5 flex flex-row flex-wrap justify-around">
+        <Card className="flex w-full max-w-sm flex-col justify-center rounded-2xl px-8 py-4">
+          <div className="flex w-full justify-center p-2">
+            <Icon size={48} icon={shieldIcon} />
+          </div>
+          <H4>
+            <FormattedMessage defaultMessage="It's fully secure" id="Admin / Card / Title" />
+          </H4>
+          <Small className="py-2">
+            <FormattedMessage defaultMessage="Regular users do not have access here" id="Admin / Card / Subtitle" />
+          </Small>
+        </Card>
+      </div>
+    </div>
+  );
+};
