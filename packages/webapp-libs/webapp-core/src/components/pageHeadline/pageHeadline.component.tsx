98da1d229112a106b64d80e88e7dad59f45532bd
Michal Kleszcz <mkleszcz@apptension.com>
2023-06-19 12:50:26 +0200

    fix: Use defined typography

--- packages/webapp-libs/webapp-core/src/components/pageHeadline/pageHeadline.component.tsx
+++ packages/webapp-libs/webapp-core/src/components/pageHeadline/pageHeadline.component.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 
 import { BackButton } from '../buttons';
 import { Separator } from '../separator';
+import { H3, Paragraph } from '../typography';
 
 export type PageHeadlineProps = {
   hasBackButton?: boolean;
@@ -14,9 +15,13 @@ export const PageHeadline = ({ hasBackButton = false, header, subheader, ...prop
     <>
       <div {...props}>
         {hasBackButton ? <BackButton className="float-right" /> : null}
-        <h3 className="text-lg font-medium">{header}</h3>
+        <H3 className="text-lg font-medium">{header}</H3>
 
-        {subheader && <p className="text-muted-foreground text-sm">{subheader}</p>}
+        {subheader && (
+          <Paragraph notFirstChildMargin={false} className="text-muted-foreground text-sm">
+            {subheader}
+          </Paragraph>
+        )}
       </div>
 
       <Separator />
