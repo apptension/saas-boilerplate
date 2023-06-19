21ef9708e952e9d2290bded49ee47278070b19cc
Jakub Bielski <jbielsky00@gmail.com>
2023-06-19 10:24:16 +0200

    feat:#174 Remove styled-componet form demoItems.component

--- packages/webapp-libs/webapp-contentful/src/routes/demoItems/demoItemListItem/demoItemListItem.component.tsx
+++ packages/webapp-libs/webapp-contentful/src/routes/demoItems/demoItemListItem/demoItemListItem.component.tsx
@@ -1,6 +1,7 @@
 import favoriteIconFilled from '@iconify-icons/ion/star';
 import favoriteIconOutlined from '@iconify-icons/ion/star-outline';
 import { DemoItemListItemFragmentFragment } from '@sb/webapp-api-client/graphql';
+import { Button, Link } from '@sb/webapp-core/components/buttons';
 import { Icon } from '@sb/webapp-core/components/icons';
 import { useGenerateLocalePath } from '@sb/webapp-core/hooks';
 import { useIntl } from 'react-intl';
@@ -8,7 +9,7 @@ import { useIntl } from 'react-intl';
 import { RoutesConfig } from '../../../config/routes';
 import { imageProps } from '../../../helpers';
 import { useFavoriteDemoItem } from '../../../hooks';
-import { Container, FavoriteIcon, Link, Thumbnail, Title } from './demoItemListItem.styles';
+import { Container, FavoriteIcon, Thumbnail, Title } from './demoItemListItem.styles';
 
 export type DemoItemListItemProps = {
   id: string;
@@ -19,28 +20,43 @@ export const DemoItemListItem = ({ id, item }: DemoItemListItemProps) => {
   const intl = useIntl();
   const { setFavorite, isFavorite } = useFavoriteDemoItem(id);
   const generateLocalePath = useGenerateLocalePath();
+  //   export const FavoriteIcon = styled.button`
+  //   padding: 0;
+  //   margin: 0;
+  //   line-height: 0;
+  //   background: none;
+  //   border: none;
+  //   color: ${color.skyBlueScale.get(50)};
+  //   cursor: pointer;
+
+  //   &:focus {
+  //     ${border.outline};
+  //   }
+  // `;
 
   return (
-    <Container>
-      <Link to={generateLocalePath(RoutesConfig.demoItem, { id })}>
-        <FavoriteIcon
-          role="checkbox"
-          aria-checked={isFavorite}
-          aria-label={intl.formatMessage({
-            defaultMessage: 'Is favorite',
-            id: 'Demo Item / Is favorite',
-          })}
-          onClick={(e) => {
-            e.preventDefault();
-            setFavorite(!isFavorite);
-          }}
-        >
-          <Icon icon={isFavorite ? favoriteIconFilled : favoriteIconOutlined} />
-        </FavoriteIcon>
-
+    <li className="flex items-center w-[100%]">
+      <Button
+        variant="ghost"
+        role="checkbox"
+        aria-checked={isFavorite}
+        aria-label={intl.formatMessage({
+          defaultMessage: 'Is favorite',
+          id: 'Demo Item / Is favorite',
+        })}
+        onClick={(e) => {
+          setFavorite(!isFavorite);
+        }}
+      >
+        <Icon icon={isFavorite ? favoriteIconFilled : favoriteIconOutlined} />
+      </Button>
+      <Link
+        className="p-8 w-[100%] justify-start hover:no-underline"
+        to={generateLocalePath(RoutesConfig.demoItem, { id })}
+      >
         {item.image && <Thumbnail {...imageProps(item.image, { size: { height: 50 } })} role="presentation" />}
-        <Title>{item.title}</Title>
+        <p>{item.title}</p>
       </Link>
-    </Container>
+    </li>
   );
 };
--- packages/webapp-libs/webapp-contentful/src/routes/demoItems/demoItems.component.tsx
+++ packages/webapp-libs/webapp-contentful/src/routes/demoItems/demoItems.component.tsx
@@ -5,19 +5,29 @@ import { FormattedMessage } from 'react-intl';
 
 import { DemoItemListItem } from './demoItemListItem';
 import { demoItemsAllQuery } from './demoItems.graphql';
-import { Container, Header, List } from './demoItems.styles';
+import { demoItemFactory } from '@sb/webapp-contentful/tests/factories';
 
 export const DemoItems = () => {
   const { data } = useQuery(demoItemsAllQuery, { context: { schemaType: SchemaType.Contentful } });
   const items = data?.demoItemCollection?.items;
 
-  return (
-    <Container>
-      <Header>
-        <FormattedMessage defaultMessage="Contentful items" id="Contentful Items / List header" />
-      </Header>
+  const item = demoItemFactory();
 
-      <List>
+  return (
+    <div className="py-4 px-12">
+      <h1 className="text-2xl mb-3 leading-6 font-bold">
+        <FormattedMessage defaultMessage="Contentful items" id="Contentful Items / List header" />
+      </h1>
+
+      <ul className="w-[100%] [&>*]:border [&>*]:border-input [&>*]:rounded [&>*]:mb-4 [&>*:last-child]:mb-0">
+        <DemoItemListItem id="1231232321" item={item} />
+        <DemoItemListItem id="1231221" item={item} />
+      </ul>
+
+      {/* <ul className="w-[100%] [&>*]:border-b [&>*]:border-input [&>*:last-child]:border-none rounded">
+        <li className="w-[100%]">dasdasdas</li>
+        <li className="w-[100%]">dasdasdas</li>
+
         {items?.map((demoItem) => {
           if (!demoItem) return null;
           return (
@@ -26,7 +36,7 @@ export const DemoItems = () => {
             </Suspense>
           );
         })}
-      </List>
-    </Container>
+      </ul> */}
+    </div>
   );
 };
