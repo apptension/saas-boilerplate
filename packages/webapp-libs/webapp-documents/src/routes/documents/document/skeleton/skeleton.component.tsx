196b1fdf0f806ca14e576197a7e44255bf2c0dfb
Jakub Bielski <jbielsky00@gmail.com>
2023-06-19 12:11:18 +0200

    feat:#174 Adjust skeleton

--- packages/webapp-libs/webapp-documents/src/routes/documents/document/skeleton/skeleton.component.tsx
+++ packages/webapp-libs/webapp-documents/src/routes/documents/document/skeleton/skeleton.component.tsx
@@ -2,11 +2,16 @@ import { Skeleton as SkeletonComponent } from '@sb/webapp-core/components/skelet
 
 export const Skeleton = () => {
   return (
-    <div className="[&>*]:mb-4 [&>*:last-child]:mb-0 flex flex-col items-center border-input border border-input p-6 rounded">
-      <SkeletonComponent className="h-5 w-12" />
-      <SkeletonComponent className="h-10 w-12" />
-      <SkeletonComponent className="h-6 w-32" />
-      <SkeletonComponent className="h-6 w-16" />
-    </div>
+    <li className="flex items-center justify-between px-3 py-6 rounded border border-input">
+      <div className="w-fit h-fit">
+        <SkeletonComponent className="h-12 w-12" />
+      </div>
+      <div className="flex justify-center w-[100%] flex-col px-4 max-w-[100%]">
+        <SkeletonComponent className="mb-1 h-5 w-[100%]" />
+
+        <SkeletonComponent className="h-4 w-8 w-[40%]" />
+      </div>
+      <SkeletonComponent className="h-8 w-8" />
+    </li>
   );
 };
