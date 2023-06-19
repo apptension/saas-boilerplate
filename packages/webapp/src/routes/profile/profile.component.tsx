1d95b1da3ea74f03e729912086424193313b7869
Kacper Adler <kadler@apptension.com>
2023-06-19 14:37:13 +0200

    fix: Other section titles in profile page

--- packages/webapp/src/routes/profile/profile.component.tsx
+++ packages/webapp/src/routes/profile/profile.component.tsx
@@ -1,7 +1,6 @@
 import { PageHeadline } from '@sb/webapp-core/components/pageHeadline';
 import { PageLayout } from '@sb/webapp-core/components/pageLayout';
-import { Separator } from '@sb/webapp-core/components/separator';
-import { H3, Paragraph, Small } from '@sb/webapp-core/components/typography';
+import { Paragraph } from '@sb/webapp-core/components/typography';
 import { FormattedMessage } from 'react-intl';
 
 import { AvatarForm } from '../../shared/components/auth/avatarForm';
@@ -56,53 +55,51 @@ export const Profile = () => {
       </div>
 
       <div className="flex w-full flex-col gap-y-6">
-        <div>
-          <H3 className="w-full text-left">
+        <PageHeadline
+          header={
             <FormattedMessage defaultMessage="Personal data" id="Auth / Profile details / Personal data header" />
-          </H3>
-          <Small className="text-muted-foreground">
+          }
+          subheader={
             <FormattedMessage
               defaultMessage="Update your account details"
               id="Auth / Profile details / Personal data label"
             />
-          </Small>
-        </div>
-        <Separator orientation="horizontal" />
+          }
+        />
         <EditProfileForm />
       </div>
 
       <div className="flex w-full flex-col gap-y-6">
-        <div>
-          <H3 className="w-full text-left">
+        <PageHeadline
+          header={
             <FormattedMessage defaultMessage="Change password" id="Auth / Profile details / Change password header" />
-          </H3>
-          <Small className="text-muted-foreground">
+          }
+          subheader={
             <FormattedMessage
               defaultMessage="Update your password"
               id="Auth / Profile details / Change password label"
             />
-          </Small>
-        </div>
-        <Separator orientation="horizontal" />
+          }
+        />
         <ChangePasswordForm />
       </div>
 
       <div className="flex w-full flex-col gap-y-6">
-        <div>
-          <H3 className="w-full text-left">
+        <PageHeadline
+          header={
             <FormattedMessage
               defaultMessage="Two-factor Authentication"
               id="Auth / Profile details / Two-factor Authentication header"
             />
-          </H3>
-          <Small className="text-muted-foreground">
+          }
+          subheader={
             <FormattedMessage
               defaultMessage="Enable 2FA on your account"
               id="Auth / Profile details / Two-factor Authentication label"
             />
-          </Small>
-        </div>
-        <Separator orientation="horizontal" />
+          }
+        />
+
         <TwoFactorAuthForm isEnabled={currentUser?.otpEnabled} />
       </div>
     </PageLayout>
