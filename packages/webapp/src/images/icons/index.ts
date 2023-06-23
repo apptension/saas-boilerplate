cdaf2eb4c81f6d243b4c085a2feb8e7be80e77a9
Kacper Adler <kadler@apptension.com>
2023-06-23 15:18:56 +0200

    feat: Remove unused icon

--- packages/webapp/src/images/icons/index.ts
+++ packages/webapp/src/images/icons/index.ts
@@ -1,7 +1,6 @@
 import { ReactComponent as FacebookImg } from './facebook.svg';
 import { ReactComponent as GoogleImg } from './google.svg';
 import { ReactComponent as HeaderLogoImg } from './headerLogo.svg';
-import { ReactComponent as HeaderLogoDarkImg } from './headerLogoDark.svg';
 import { makeIcon } from './makeIcon';
 
 //<-- IMPORT ICON FILE -->
@@ -9,5 +8,4 @@ import { makeIcon } from './makeIcon';
 export const FacebookIcon = makeIcon(FacebookImg);
 export const GoogleIcon = makeIcon(GoogleImg);
 export const HeaderLogoIcon = makeIcon(HeaderLogoImg);
-export const HeaderLogoDarkIcon = makeIcon(HeaderLogoDarkImg);
 //<-- EXPORT ICON COMPONENT -->
