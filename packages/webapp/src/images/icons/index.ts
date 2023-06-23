808b4e8b00d4e4bc48354a498e7b5c500119709d
Kacper Adler <kadler@apptension.com>
2023-06-23 14:43:14 +0200

    feat: Update app logo

--- packages/webapp/src/images/icons/index.ts
+++ packages/webapp/src/images/icons/index.ts
@@ -1,6 +1,7 @@
 import { ReactComponent as FacebookImg } from './facebook.svg';
 import { ReactComponent as GoogleImg } from './google.svg';
 import { ReactComponent as HeaderLogoImg } from './headerLogo.svg';
+import { ReactComponent as HeaderLogoDarkImg } from './headerLogoDark.svg';
 import { makeIcon } from './makeIcon';
 
 //<-- IMPORT ICON FILE -->
@@ -8,4 +9,5 @@ import { makeIcon } from './makeIcon';
 export const FacebookIcon = makeIcon(FacebookImg);
 export const GoogleIcon = makeIcon(GoogleImg);
 export const HeaderLogoIcon = makeIcon(HeaderLogoImg);
+export const HeaderLogoDarkIcon = makeIcon(HeaderLogoDarkImg);
 //<-- EXPORT ICON COMPONENT -->
