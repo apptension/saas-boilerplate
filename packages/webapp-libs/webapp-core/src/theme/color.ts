511cf7a5a3cf48478973c3fc03c7f299d4b5ef59
Kacper Adler <kadler@apptension.com>
2023-06-23 10:38:45 +0200

    feat: Remove unused variables from theme

--- packages/webapp-libs/webapp-core/src/theme/color.ts
+++ packages/webapp-libs/webapp-core/src/theme/color.ts
@@ -1,75 +1,12 @@
 import { colorScale } from './utils/colorScale';
 
 export const white = '#ffffff';
-export const black = '#000000';
 
 export const greyScale = colorScale('#808080');
 export const skyBlueScale = colorScale('#0080FF');
-export const ultramarineBlueScale = colorScale('#0001FF');
-export const prussianBlueScale = colorScale('#406ABF');
 export const cherryRedScale = colorScale('#FF0055');
-
-export const text = greyScale.get(15);
-export const border = '#80809B';
-export const disabled = '#DDDDE0';
-export const primary = skyBlueScale.get(50);
-export const secondary = '#2249f9';
-export const error = cherryRedScale.get(50);
 
 export const button = {
   main: skyBlueScale.get(50),
   text: white,
-};
-
-export const checkbox = {
-  idle: greyScale.get(55),
-  icon: white,
-  main: skyBlueScale.get(35),
-  active: skyBlueScale.get(50),
-  hover: skyBlueScale.get(65),
-  invalid: error,
-};
-
-export const input = {
-  text: greyScale.get(15),
-  label: greyScale.get(15),
-  border: greyScale.get(55),
-  background: white,
-
-  disabled: {
-    text: greyScale.get(70),
-    label: greyScale.get(40),
-    border: greyScale.get(90),
-    background: greyScale.get(98),
-  },
-
-  hover: skyBlueScale.get(65),
-  active: skyBlueScale.get(50),
-  invalid: cherryRedScale.get(50),
-};
-
-export const radio = {
-  main: skyBlueScale.get(50),
-  hover: {
-    main: skyBlueScale.get(98),
-    border: skyBlueScale.get(65),
-  },
-  disabled: {
-    main: greyScale.get(90),
-    text: greyScale.get(70),
-  },
-};
-
-export const listItem = {
-  background: white,
-  hover: {
-    background: skyBlueScale.get(98),
-  },
-  focus: {
-    background: skyBlueScale.get(98),
-  },
-  active: {
-    background: skyBlueScale.get(95),
-    text: skyBlueScale.get(35),
-  },
 };
--- packages/webapp-libs/webapp-core/src/theme/size.ts
+++ packages/webapp-libs/webapp-core/src/theme/size.ts
@@ -1,20 +1,9 @@
-import styled, { css } from 'styled-components';
-import { Breakpoint, media, responsiveValue } from './media';
+import { css } from 'styled-components';
+
+import { Breakpoint, responsiveValue } from './media';
 
 export const sizeUnitBase = 8;
 export const sizeUnits = (size = 1) => size * sizeUnitBase + 'px';
-
-export const header = responsiveValue(sizeUnits(7), {
-  [Breakpoint.TABLET]: sizeUnits(8),
-});
-
-export const sideMenu = '281px';
-
-export const fullContentHeight = css`
-  ${media(Breakpoint.TABLET)`
-    min-height: calc(100vh - ${header});
-  `};
-`;
 
 export const contentHorizontalPadding = responsiveValue(sizeUnits(2), {
   [Breakpoint.TABLET]: sizeUnits(4),
@@ -24,60 +13,12 @@ export const smallContentHorizontalPadding = responsiveValue(sizeUnits(1), {
   [Breakpoint.TABLET]: sizeUnits(2),
 });
 
-export const contentVerticalPadding = responsiveValue(sizeUnits(1), {
-  [Breakpoint.TABLET]: sizeUnits(1.5),
-});
-
-export const contentWithLimitedWidth = css`
-  max-width: 684px;
-`;
-
-export const contentWrapper = css`
-  width: 100%;
-  padding-left: ${sizeUnits(2)};
-  padding-right: ${sizeUnits(2)};
-  margin-left: auto;
-  margin-right: auto;
-
-  ${media(Breakpoint.TABLET)`
-    padding-left: ${sizeUnits(5)};
-    padding-right: ${sizeUnits(5)};
-  `};
-`;
-
 export const horizontalPadding = (value: string) => css`
   padding-left: ${value};
   padding-right: ${value};
-`;
-
-export const verticalPadding = (value: string) => css`
-  padding-top: ${value};
-  padding-bottom: ${value};
 `;
 
 export const horizontalMargin = (value: string) => css`
   margin-left: ${value};
   margin-right: ${value};
 `;
-
-export const verticalMargin = (value: string) => css`
-  margin-top: ${value};
-  margin-bottom: ${value};
-`;
-
-export const formFieldWidth = css`
-  width: 100%;
-  max-width: 342px;
-
-  ${media(Breakpoint.TABLET)`
-    max-width: 288px;
-  `};
-`;
-
-export const FormFieldsRow = styled.div`
-  & + & {
-    margin-top: ${sizeUnits(3)};
-  }
-`;
-
-export const formVerticalFieldsGutter = sizeUnits(3);
