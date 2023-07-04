2eb51cc68925a4439932d24ef4493c5dd5142dce
Kacper Adler <kadler@apptension.com>
2023-07-04 16:39:48 +0200

    fix: Tests

--- packages/webapp-libs/webapp-core/src/theme/color.ts
+++ packages/webapp-libs/webapp-core/src/theme/color.ts
@@ -3,6 +3,8 @@ import { colorScale } from './utils/colorScale';
 export const white = '#ffffff';
 
 export const greyScale = colorScale('#808080');
+export const skyBlueScale = colorScale('#0080FF');
+export const cherryRedScale = colorScale('#FF0055');
 
 export const button = {
   main: 'hsl(222.2 47.4% 11.2%)',
