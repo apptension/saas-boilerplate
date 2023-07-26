3a8fab6d52719902157f20c7f0a3085ecf8ef26c
Patryk Ziemkowski <pziemkowski@apptension.com>
2023-07-26 14:05:12 +0200

    fix: Fix download-graphql-schema.sh in webapp-api-client and webapp-contentful (#350)

--- packages/webapp-libs/webapp-api-client/scripts/download-graphql-schema.sh
+++ packages/webapp-libs/webapp-api-client/scripts/download-graphql-schema.sh
@@ -7,4 +7,4 @@ API_URL="http://localhost:5001/api/graphql/"
 rm  -f ./src/graphql/__generated/types.ts
 rm  -f ./src/graphql/__generated/hooks.ts
 
-./node_modules/.bin/rover graph introspect "$API_URL" --output graphql/schema/api.graphql
+pnpm rover graph introspect "$API_URL" --output graphql/schema/api.graphql
