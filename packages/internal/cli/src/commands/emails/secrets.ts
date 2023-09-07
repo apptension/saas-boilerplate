8685105f1a7e0ed637db78fcec91d8491a33a807
Patryk Ziemkowski <pziemkowski@apptension.com>
2023-09-07 11:14:09 +0200

    Migrate commands from emails to CLI

---
+++ packages/internal/cli/src/commands/emails/secrets.ts
@@ -0,0 +1,15 @@
+import { Command } from '@oclif/core';
+
+export default class EmailsSecrets extends Command {
+  static description =
+    'Runs an ssm-editor helper tool in docker container to set runtime environmental variables of webapp service. ' +
+    'Underneath it uses chamber to both fetch and set those variables in AWS SSM Parameter Store';
+
+  static examples = [`$ <%= config.bin %> <%= command.id %>`];
+
+  async run(): Promise<void> {
+    this.error(
+      'Emails package do not have their own separate secrets service. Use `saas webapp secrets` instead.'
+    );
+  }
+}
--- packages/internal/cli/src/commands/emails/test.ts
+++ packages/internal/cli/src/commands/emails/test.ts
@@ -3,14 +3,14 @@ import { Command } from '@oclif/core';
 import { initConfig } from '../../config/init';
 import { runCommand } from '../../lib/runCommand';
 
-export default class WebappTest extends Command {
-  static description = 'Runs all webapp tests';
+export default class EmailsTest extends Command {
+  static description = 'Runs all emails tests';
 
   static examples = [`$ <%= config.bin %> <%= command.id %>`];
 
   async run(): Promise<void> {
     await initConfig(this, {});
 
-    await runCommand('pnpm', ['nx', 'run', 'webapp:test']);
+    await runCommand('pnpm', ['nx', 'run', 'webapp-emails:test']);
   }
 }
