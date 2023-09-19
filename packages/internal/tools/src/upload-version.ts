4b393399554600098d0922c71f05bfe9a36d0d98
Patryk Ziemkowski <pziemkowski@apptension.com>
2023-09-19 15:47:38 +0200

    chore: Fix moderate dependency security issues flagged by dependabot (#390)
    * Upgrade aws-sdk to v3 in @sb/tools
    
    * Update pnpm-lock.yaml
    
    * Remove mocha from CLI

--- packages/internal/tools/src/upload-version.ts
+++ packages/internal/tools/src/upload-version.ts
@@ -1,4 +1,8 @@
-import * as AWS from 'aws-sdk';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
 
 const ENV_STAGE = process.env.ENV_STAGE;
 const CURRENT_VERSION = process.env.VERSION;
@@ -9,62 +13,63 @@ const VERSIONS_OBJECT = 'versions.json';
 const [servicesArgv, ...valuesArgv] = process.argv.slice(2);
 
 const values = valuesArgv.reduce((values, arg) => {
-    const [label, value] = arg.split('=');
+  const [label, value] = arg.split('=');
 
-    return [...values, { label, value }];
+  return [...values, { label, value }];
 }, []);
 
 const services = servicesArgv.split(',');
 
-const s3 = new AWS.S3();
+const s3Client = new S3Client();
 
 const newVersion = {
-    name: ENV_STAGE,
-    version: CURRENT_VERSION,
-    builtAt: new Date(),
-    services,
-    values,
+  name: ENV_STAGE,
+  version: CURRENT_VERSION,
+  builtAt: new Date(),
+  services,
+  values,
 };
 
+type NewVersion = typeof newVersion;
+
 function putJsonObject(payload) {
-    s3.putObject({
-        Bucket: VERSIONS_BUCKET,
-        Key: VERSIONS_OBJECT,
-        CacheControl: 'no-cache',
-        ACL: 'public-read',
-        ContentType: 'application/json',
-        Body: JSON.stringify(payload),
-    }, (error) => {
-        if (error) {
-            console.log(error);
-        }
-    });
+  return s3Client.send(
+    new PutObjectCommand({
+      Bucket: VERSIONS_BUCKET,
+      Key: VERSIONS_OBJECT,
+      CacheControl: 'no-cache',
+      ACL: 'public-read',
+      ContentType: 'application/json',
+      Body: JSON.stringify(payload),
+    })
+  );
 }
 
-s3.getObject(
-    { Bucket: VERSIONS_BUCKET, Key: VERSIONS_OBJECT },
-    function (error, data) {
-        if (error != null && error.statusCode === 404) {
-            putJsonObject([newVersion]);
-        } else {
-            const versions = JSON.parse(data?.Body?.toString('utf-8') ?? '{}');
-            const newVersions: any[] = [];
+(async () => {
+  try {
+    const data = await s3Client.send(
+      new GetObjectCommand({ Bucket: VERSIONS_BUCKET, Key: VERSIONS_OBJECT })
+    );
 
-            let found = false;
-            for (const version of versions) {
-                if (version.name === ENV_STAGE) {
-                    newVersions.push(newVersion);
-                    found = true;
-                } else {
-                    newVersions.push(version);
-                }
-            }
+    const versions = JSON.parse(data.Body?.toString() ?? '{}');
+    const newVersions: NewVersion[] = [];
 
-            if (!found) {
-                newVersions.push(newVersion);
-            }
-
-            putJsonObject(newVersions);
-        }
+    let found = false;
+    for (const version of versions) {
+      if (version.name === ENV_STAGE) {
+        newVersions.push(newVersion);
+        found = true;
+      } else {
+        newVersions.push(version);
+      }
     }
-);
+
+    if (!found) {
+      newVersions.push(newVersion);
+    }
+
+    await putJsonObject(newVersions);
+  } catch (error) {
+    await putJsonObject([newVersion]);
+  }
+})();
