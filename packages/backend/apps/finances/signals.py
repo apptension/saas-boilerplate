d4bb64dc8836e09bdb4d28c88e90a61c7e06db99
Patryk Ziemkowski <pziemkowski@apptension.com>
2023-09-25 12:30:38 +0200

    fix: Fix error thrown when registering a new user when stripe is disabled

--- packages/backend/apps/finances/signals.py
+++ packages/backend/apps/finances/signals.py
@@ -3,6 +3,7 @@ import logging
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 from stripe.error import AuthenticationError
 
 from .services import subscriptions
@@ -15,6 +16,9 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_free_plan_subscription(sender, instance, created, **kwargs):
+    if not settings.STRIPE_ENABLED:
+        return
+
     if created:
         try:
             subscriptions.initialize_user(user=instance)
