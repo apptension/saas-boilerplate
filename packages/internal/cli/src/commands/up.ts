2c542ac8c31ec8e3f44e85fe8219dad351eadec2
Patryk Ziemkowski <pziemkowski@apptension.com>
2023-10-23 13:45:37 +0200

    feat: Add a waiting mechanism to `saas up` CLI command to wait for backend to start before starting web app dev server (#420)

--- packages/internal/cli/src/commands/up.ts
+++ packages/internal/cli/src/commands/up.ts
@@ -1,7 +1,87 @@
+import { Command, ux } from '@oclif/core';
+import * as util from 'node:util';
+import { exec as _exec } from 'node:child_process';
+
 import { initConfig } from '../config/init';
 import { runCommand } from '../lib/runCommand';
 import { assertDockerIsRunning, dockerHubLogin } from '../lib/docker';
 import { BaseCommand } from '../baseCommand';
+
+const exec = util.promisify(_exec);
+
+type Publisher = {
+  URL: string;
+  PublishedPort: number;
+};
+
+type DockerComposePsResult = Array<{
+  Project: string;
+  Service: string;
+  Publishers: Array<Publisher> | null;
+}>;
+
+async function getBackendEndpoint(context: Command) {
+  const expectedBackendPort = 5001;
+  const { stdout: psResultStr } = await exec('docker compose ps --format=json');
+  const psResult = JSON.parse(psResultStr) as DockerComposePsResult;
+
+  const backendContainerExists = psResult.some(
+    ({ Service: service }) => service === 'backend',
+  );
+
+  if (!backendContainerExists) {
+    context.error('running backend container not found');
+  }
+
+  const hasExpectedPort = ({ PublishedPort }: Publisher) =>
+    PublishedPort === expectedBackendPort;
+  const backendContainer = psResult.find(
+    ({ Service: service, Publishers: publishers }) =>
+      service === 'backend' && publishers?.some(hasExpectedPort),
+  );
+
+  if (!backendContainer) {
+    context.error(
+      `backend container does not expose expected port ${expectedBackendPort}. Web app will not be able to send requests
+to API. Make sure that backend service in docker-compose.yml exposes ${expectedBackendPort}. Read more on
+docker compose networking in official documentation: https://docs.docker.com/compose/networking/`,
+    );
+  }
+
+  const publisher = backendContainer.Publishers?.find(hasExpectedPort);
+  return `http://${publisher?.URL}:${publisher?.PublishedPort}`;
+}
+
+type WaitForBackendOptions = {
+  url: string;
+  retryCount: number;
+  stepTime?: number;
+};
+
+async function waitForBackend(
+  context: Command,
+  { url, stepTime = 500, retryCount }: WaitForBackendOptions,
+) {
+  ux.action.start(
+    'Backend server is not ready yet.\n You can run `docker compose logs backend -f` ' +
+      'to see logs from backend container.\n Waiting',
+  );
+
+  for (let i = 0; i < retryCount; i += 1) {
+    try {
+      await fetch(url, { method: 'GET' });
+      ux.action.stop();
+      return;
+    } catch (error) {
+      await new Promise((resolve) => setTimeout(resolve, stepTime));
+    }
+  }
+
+  context.error(
+    'Timeout: Backend dev server failed to start. Run `docker compose logs backend -f` to understand why it' +
+      'happened',
+  );
+}
 
 export default class Up extends BaseCommand<typeof Up> {
   static description = 'Starts both backend and frontend';
@@ -14,6 +94,8 @@ export default class Up extends BaseCommand<typeof Up> {
     await dockerHubLogin();
 
     await runCommand('pnpm', ['nx', 'run', 'core:docker-compose:up']);
+    const backendEndpoint = await getBackendEndpoint(this);
+    await waitForBackend(this, { url: backendEndpoint, retryCount: 200 });
     await runCommand('pnpm', ['nx', 'run', 'webapp:start']);
   }
 }
