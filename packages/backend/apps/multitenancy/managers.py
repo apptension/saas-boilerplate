e1da630a8beb2be61dfbe0bbde0f254f44053b0f
Mateusz Wójcik <mateusz.wojcik@apptension.com>
2024-02-14 11:23:05 +0100

    Add short documentation for multi tenancy manager

--- packages/backend/apps/multitenancy/managers.py
+++ packages/backend/apps/multitenancy/managers.py
@@ -5,6 +5,15 @@ from .constants import TenantType
 
 class TenantManager(models.Manager):
     def get_or_create_user_sign_up_tenant(self, user):
+        """
+        Description:
+        Retrieves or creates a tenant for a given user to ensure there is always at least one tenant associated with the user.
+        Parameters:
+        - user (User): The user for whom the tenant is retrieved or created.
+
+        Returns:
+        Tenant: The associated or newly created tenant instance of SIGN_UP type.
+        """
         sign_up_tenant = self.filter(creator=user, type=TenantType.SIGN_UP).order_by('created').first()
         if sign_up_tenant:
             return sign_up_tenant, False
