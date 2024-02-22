fed8fb88be752fd7d6631f4f6d7b14c67ca0d38f
Mateusz Wójcik <mateusz.wojcik@apptension.com>
2024-02-22 12:42:59 +0100

    Add documentation

--- packages/backend/apps/multitenancy/models.py
+++ packages/backend/apps/multitenancy/models.py
@@ -10,6 +10,28 @@ from common.models import TimestampedMixin
 
 
 class Tenant(TimestampedMixin, models.Model):
+    """
+    Represents a tenant within the application.
+
+    Fields:
+    - id: A unique identifier for the tenant.
+    - creator: The user who created the tenant.
+    - name: The name of the tenant.
+    - slug: A URL-friendly version of the name.
+    - type: The type of the tenant.
+    - members: Many-to-many relationship with users through TenantMembership.
+
+    Methods:
+    - save: Overrides the default save method to ensure unique slug generation based on the name field.
+
+    Initialization:
+    - __original_name: Private attribute to track changes to the name field during the instance's lifecycle.
+
+    Slug Generation:
+    - The save method ensures the generation of a unique slug for the tenant. If the name is modified or the slug is
+      not provided, it generates a slug based on the name. In case of a name collision, a counter is appended to the
+      base slug to ensure uniqueness.
+    """
     id: str = hashid_field.HashidAutoField(primary_key=True)
     creator: settings.AUTH_USER_MODEL = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name: str = models.CharField(max_length=100, unique=False)
