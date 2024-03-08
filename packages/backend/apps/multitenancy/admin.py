c4d4653f0967af38437e304fc5fcf118efa325bb
wojcikmat <mateusz.wojcik@apptension.com>
2024-03-08 16:30:37 +0100

    feat: Add admin panel for multitenancy (#499)

--- packages/backend/apps/multitenancy/admin.py
+++ packages/backend/apps/multitenancy/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "type")
+
+
+@admin.register(models.TenantMembership)
+class TenantMembershipAdmin(admin.ModelAdmin):
+    list_display = ("id", "role", "user", "invitee_email_address", "tenant", "is_accepted")
