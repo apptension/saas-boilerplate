f846e4ded58f3aad39cc33a363286bb038444a03
Mateusz Skwierczyński <mskwierczynski@apptension.com>
2024-04-08 11:48:57 +0200

    chore: update tenant membership graphql adjustments

--- packages/webapp-libs/webapp-tenants/src/components/tenantMembersList/membershipEntry/membershipEntry.component.tsx
+++ packages/webapp-libs/webapp-tenants/src/components/tenantMembersList/membershipEntry/membershipEntry.component.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from '@apollo/client';
 import { TenantMembershipType, TenantUserRole } from '@sb/webapp-api-client';
 import { Button } from '@sb/webapp-core/components/buttons';
 import {
@@ -17,29 +18,21 @@ import { indexBy, prop, trim } from 'ramda';
 import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import { useMutation } from '@apollo/client';
 import { useTenantRoles } from '../../../hooks/useTenantRoles';
+import { useCurrentTenant } from '../../../providers';
 import { updateTenantMembershipMutation } from './membershipEntry.graphql';
-import { useCurrentTenant } from '@sb/webapp-tenants/providers';
 
 export type MembershipEntryProps = {
   className?: string;
   membership: TenantMembershipType;
 };
 
-export const MembershipEntry = ({
-  membership,
-  className,
-}: MembershipEntryProps) => {
+export const MembershipEntry = ({ membership, className }: MembershipEntryProps) => {
   const { data: currentTenant } = useCurrentTenant();
   const { getRoleTranslation } = useTenantRoles();
   const [commitUpdateMutation] = useMutation(updateTenantMembershipMutation);
 
-  const roles = [
-    TenantUserRole.OWNER,
-    TenantUserRole.ADMIN,
-    TenantUserRole.MEMBER,
-  ];
+  const roles = [TenantUserRole.OWNER, TenantUserRole.ADMIN, TenantUserRole.MEMBER];
   const roleChangeCallbacks = useMemo(() => {
     if (!currentTenant) return;
 
@@ -56,35 +49,21 @@ export const MembershipEntry = ({
     return indexBy(prop('role'), mapper);
   }, [roles, membership.id]);
 
-  const name = trim(
-    [membership.firstName, membership.lastName]
-      .map((s) => trim(s ?? ''))
-      .join(' '),
-  );
+  const name = trim([membership.firstName, membership.lastName].map((s) => trim(s ?? '')).join(' '));
   return (
     <TableRow className={className}>
-      <TableCell>
-        {name || membership.userEmail || membership.inviteeEmailAddress}
-      </TableCell>
-      <TableCell>
-        {getRoleTranslation(membership.role?.toUpperCase() as TenantUserRole)}
-      </TableCell>
+      <TableCell>{name || membership.userEmail || membership.inviteeEmailAddress}</TableCell>
+      <TableCell>{getRoleTranslation(membership.role?.toUpperCase() as TenantUserRole)}</TableCell>
       <TableCell>
         {membership.invitationAccepted ? (
           <div className="flex items-center">
             <UserCheck className="mr-2 w-4 h-4" />
-            <FormattedMessage
-              id="Tenant Membersip Entry / Yes"
-              defaultMessage="Yes"
-            />
+            <FormattedMessage id="Tenant Membersip Entry / Yes" defaultMessage="Yes" />
           </div>
         ) : (
           <div className="flex items-center">
             <Hourglass className="mr-2 w-4 h-4" />
-            <FormattedMessage
-              id="Tenant Membersip Entry / Yes"
-              defaultMessage="No"
-            />
+            <FormattedMessage id="Tenant Membersip Entry / Yes" defaultMessage="No" />
           </div>
         )}
       </TableCell>
@@ -97,27 +76,18 @@ export const MembershipEntry = ({
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>
-              <FormattedMessage
-                id="Tenant Membersip Entry / Actions"
-                defaultMessage="Actions"
-              />
+              <FormattedMessage id="Tenant Membersip Entry / Actions" defaultMessage="Actions" />
             </DropdownMenuLabel>
             <DropdownMenuSub>
               <DropdownMenuSubTrigger>
-                <FormattedMessage
-                  id="Tenant Membersip Entry / Change role"
-                  defaultMessage="Change role"
-                />
+                <FormattedMessage id="Tenant Membersip Entry / Change role" defaultMessage="Change role" />
               </DropdownMenuSubTrigger>
               <DropdownMenuPortal>
                 <DropdownMenuSubContent>
                   {roles
                     .filter((role) => role !== (membership.role?.toUpperCase() as TenantUserRole))
                     .map((role) => (
-                      <DropdownMenuItem
-                        onClick={roleChangeCallbacks[role].callback}
-                        key={role}
-                      >
+                      <DropdownMenuItem onClick={roleChangeCallbacks?.[role].callback} key={role}>
                         {getRoleTranslation(role)}
                       </DropdownMenuItem>
                     ))}
@@ -125,10 +95,7 @@ export const MembershipEntry = ({
               </DropdownMenuPortal>
             </DropdownMenuSub>
             <DropdownMenuItem>
-              <FormattedMessage
-                id="Tenant Membersip Entry / Delete"
-                defaultMessage="Delete"
-              />
+              <FormattedMessage id="Tenant Membersip Entry / Delete" defaultMessage="Delete" />
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
--- packages/webapp-libs/webapp-tenants/src/components/tenantMembersList/membershipEntry/membershipEntry.graphql.ts
+++ packages/webapp-libs/webapp-tenants/src/components/tenantMembersList/membershipEntry/membershipEntry.graphql.ts
@@ -1,20 +1,10 @@
 import { gql } from '@sb/webapp-api-client/graphql';
 
 export const updateTenantMembershipMutation = gql(/* GraphQL */ `
-  mutation updateTenantMembershipMutation(
-    $input: UpdateTenantMembershipMutationInput!
-  ) {
+  mutation updateTenantMembershipMutation($input: UpdateTenantMembershipMutationInput!) {
     updateTenantMembership(input: $input) {
       tenantMembership {
         id
-        role
-        invitationAccepted
-        inviteeEmailAddress
-        userId
-        firstName
-        lastName
-        userEmail
-        avatar
       }
     }
   }
