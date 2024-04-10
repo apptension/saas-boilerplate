ca9129ef6e94d6d5337aa3cf563b1d81782c0530
Mateusz Skwierczyński <mskwierczynski@apptension.com>
2024-04-10 16:14:59 +0200

    chore: update unit test

--- packages/webapp-libs/webapp-tenants/src/routes/tenantSettings/tenantGeneralSettings/__tests__/tenantGeneralSettings.component.spec.tsx
+++ packages/webapp-libs/webapp-tenants/src/routes/tenantSettings/tenantGeneralSettings/__tests__/tenantGeneralSettings.component.spec.tsx
@@ -1,3 +1,5 @@
+import { TenantType as TenantTypeField } from '@sb/webapp-api-client/constants';
+import { commonQueryCurrentUserQuery } from '@sb/webapp-api-client/providers';
 import { currentUserFactory, fillCommonQueryWithUser } from '@sb/webapp-api-client/tests/factories';
 import { composeMockedQueryResult } from '@sb/webapp-api-client/tests/utils';
 import { screen } from '@testing-library/react';
@@ -24,7 +26,7 @@ describe('TenantGeneralSettings: Component', () => {
     const commonQueryMock = fillCommonQueryWithUser(user);
 
     const variables = {
-      input: { id: '1', name: 'new item name' },
+      input: { id: '1', name: 'name - new item name' },
     };
 
     const data = {
@@ -38,15 +40,37 @@ describe('TenantGeneralSettings: Component', () => {
       data,
     });
 
+    const currentUserRefetchData = {
+      ...user,
+      tenants: [
+        ...(user.tenants ?? []),
+        tenantFactory({
+          id: '1',
+          name: variables.input.name,
+          type: TenantTypeField.ORGANIZATION,
+        }),
+      ],
+    };
+
+    const refetchMock = composeMockedQueryResult(commonQueryCurrentUserQuery, {
+      data: currentUserRefetchData,
+    });
+
     requestMock.newData = jest.fn(() => ({
       data,
     }));
 
+    refetchMock.newData = jest.fn(() => ({
+      data: {
+        currentUser: currentUserRefetchData,
+      },
+    }));
+
     const routerProps = createMockRouterProps(RoutesConfig.tenant.settings.general, { tenantId: '1' });
 
-    render(<Component />, { apolloMocks: [commonQueryMock, requestMock], routerProps });
+    render(<Component />, { apolloMocks: [commonQueryMock, requestMock, refetchMock], routerProps });
 
-    await userEvent.type(await screen.findByPlaceholderText(/name/i), 'new item name');
+    await userEvent.type(await screen.findByPlaceholderText(/name/i), ' - new item name');
     await userEvent.click(screen.getByRole('button', { name: /save/i }));
     expect(requestMock.newData).toHaveBeenCalled();
     const toast = await screen.findByTestId('toast-1');
