637abb4d25d516171df49907e9611ab4aaf18b49
Sebastian Drejkarz <sdrejkarz@apptension.com>
2024-05-13 14:35:56 +0200

    feat: reload common query on notification click

--- packages/webapp-libs/webapp-api-client/src/providers/commonQuery/commonQuery.context.ts
+++ packages/webapp-libs/webapp-api-client/src/providers/commonQuery/commonQuery.context.ts
@@ -4,12 +4,12 @@ import { CommonQueryCurrentUserQueryQuery } from '../../graphql';
 
 type CommonDataContext = {
   data: CommonQueryCurrentUserQueryQuery | null;
-  reload: () => void;
+  reload: () => Promise<void>;
 };
 
 export default React.createContext<CommonDataContext>({
   data: null,
-  reload: () => {
+  reload: async () => {
     return;
   },
 });
--- packages/webapp-libs/webapp-tenants/src/notifications/tenantInvitationCreated/tenantInvitationCreated.component.tsx
+++ packages/webapp-libs/webapp-tenants/src/notifications/tenantInvitationCreated/tenantInvitationCreated.component.tsx
@@ -1,6 +1,8 @@
+import { useCommonQuery } from '@sb/webapp-api-client/providers';
 import { RoutesConfig } from '@sb/webapp-core/config/routes';
 import { useGenerateLocalePath } from '@sb/webapp-core/hooks';
 import { Notification, NotificationType } from '@sb/webapp-notifications';
+import { useCallback, useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useNavigate } from 'react-router';
 
@@ -18,20 +20,39 @@ export const TenantInvitationCreated = ({
 }: TenantInvitationCreatedProps) => {
   const generateLocalePath = useGenerateLocalePath();
   const navigate = useNavigate();
+  const { reload: reloadCommonQuery } = useCommonQuery();
+  const [shouldNavigateWithSideEffect, setSideEffectNavigation] = useState(false);
 
   const tenants = useTenants();
 
-  const handleInvitationClick = () => {
-    const tenant = tenants.find((tenant) => tenant?.membership?.id === id);
-    const token = tenant?.membership?.invitationToken;
-    if (!token) return;
-    navigate(generateLocalePath(RoutesConfig.tenantInvitation, { token }));
-  };
+  const handleInvitationClick = useCallback(
+    (initialClick: boolean = false) => {
+      const tenant = tenants.find((tenant) => tenant?.membership?.id === id);
+
+      if (!tenant) {
+        if (initialClick) reloadCommonQuery().then(() => setSideEffectNavigation(true));
+        return;
+      }
+
+      const token = tenant.membership.invitationToken;
+      if (!token) return;
+
+      navigate(generateLocalePath(RoutesConfig.tenantInvitation, { token }));
+    },
+    [generateLocalePath, id, navigate, reloadCommonQuery, tenants]
+  );
+
+  useEffect(() => {
+    if (shouldNavigateWithSideEffect) {
+      handleInvitationClick(true);
+      setSideEffectNavigation(false);
+    }
+  }, [handleInvitationClick, shouldNavigateWithSideEffect]);
 
   return (
     <Notification
       {...restProps}
-      onClick={handleInvitationClick}
+      onClick={() => handleInvitationClick(true)}
       avatar={issuer?.avatar}
       title={issuer?.email}
       content={
