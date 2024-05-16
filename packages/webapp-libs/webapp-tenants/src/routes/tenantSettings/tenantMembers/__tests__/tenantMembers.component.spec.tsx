754c6d1454be325d15cee4d5ee01171fe95d7d9e
Sebastian Drejkarz <sdrejkarz@apptension.com>
2024-05-16 16:12:39 +0200

    feat: add additional info on personals members tab

---
+++ packages/webapp-libs/webapp-tenants/src/routes/tenantSettings/tenantMembers/__tests__/tenantMembers.component.spec.tsx
@@ -0,0 +1,65 @@
+import { TenantType } from '@sb/webapp-api-client/constants';
+import { currentUserFactory, fillCommonQueryWithUser } from '@sb/webapp-api-client/tests/factories';
+import { Tabs } from '@sb/webapp-core/components/tabs';
+import { screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+
+import { RoutesConfig } from '../../../../config/routes';
+import { tenantFactory } from '../../../../tests/factories/tenant';
+import { createMockRouterProps, render } from '../../../../tests/utils/rendering';
+import { TenantMembers } from '../tenantMembers.component';
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+  return {
+    ...jest.requireActual<NodeModule>('react-router-dom'),
+    useNavigate: () => mockNavigate,
+  };
+});
+
+const tenantId = 'testId';
+
+describe('TenantMembers: Component', () => {
+  const Component = () => (
+    <Tabs value={`/en/${tenantId}/tenant/settings/members`}>
+      <TenantMembers />
+    </Tabs>
+  );
+
+  it('should render alert for personal account', async () => {
+    const tenant = tenantFactory({ id: tenantId, type: TenantType.PERSONAL });
+    const routerProps = createMockRouterProps(RoutesConfig.tenant.settings.general, { tenantId });
+
+    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants: [tenant] }))];
+
+    render(<Component />, { apolloMocks, routerProps });
+
+    expect(await screen.findByTestId('tenant-members-alert')).toBeInTheDocument();
+    expect(await screen.findByTestId('tenant-members-create-button')).toBeInTheDocument();
+  });
+
+  it('should handle new tenant click', async () => {
+    const tenant = tenantFactory({ id: tenantId, type: TenantType.PERSONAL });
+    const routerProps = createMockRouterProps(RoutesConfig.tenant.settings.general, { tenantId });
+
+    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants: [tenant] }))];
+
+    render(<Component />, { apolloMocks, routerProps });
+
+    const newTenantButton = await screen.findByTestId('tenant-members-create-button');
+    await userEvent.click(newTenantButton);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/en/add-tenant');
+  });
+
+  it('should render members with invitation form', async () => {
+    const tenant = tenantFactory({ id: tenantId, type: TenantType.ORGANIZATION });
+    const routerProps = createMockRouterProps(RoutesConfig.tenant.settings.general, { tenantId });
+
+    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants: [tenant] }))];
+
+    render(<Component />, { apolloMocks, routerProps });
+
+    expect(await screen.findByTestId('tenant-members-list')).toBeInTheDocument();
+  });
+});
--- packages/webapp-libs/webapp-tenants/src/routes/tenantSettings/tenantMembers/tenantMembers.component.tsx
+++ packages/webapp-libs/webapp-tenants/src/routes/tenantSettings/tenantMembers/tenantMembers.component.tsx
@@ -1,14 +1,30 @@
+import { TenantType } from '@sb/webapp-api-client/constants';
+import { Alert } from '@sb/webapp-core/components/alert';
+import { Button } from '@sb/webapp-core/components/buttons';
 import { PageHeadline } from '@sb/webapp-core/components/pageHeadline';
 import { TabsContent } from '@sb/webapp-core/components/tabs';
+import { RoutesConfig as RootRoutesConfig } from '@sb/webapp-core/config/routes';
+import { useGenerateLocalePath } from '@sb/webapp-core/hooks';
+import { Paragraph } from '@sb/webapp-core/theme/typography';
+import { Plus } from 'lucide-react';
 import { FormattedMessage } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
 
 import { TenantMembersList } from '../../../components/tenantMembersList';
 import { RoutesConfig } from '../../../config/routes';
 import { useGenerateTenantPath } from '../../../hooks';
+import { useCurrentTenant } from '../../../providers';
 import { InvitationForm } from './invitationForm';
 
 export const TenantMembers = () => {
   const generateTenantPath = useGenerateTenantPath();
+  const generateLocalePath = useGenerateLocalePath();
+  const navigate = useNavigate();
+  const isPersonal = useCurrentTenant().data?.type === TenantType.PERSONAL;
+
+  const handleLastInvitationClick = () => {
+    navigate(generateLocalePath(RootRoutesConfig.addTenant));
+  };
 
   return (
     <TabsContent value={generateTenantPath(RoutesConfig.tenant.settings.members)}>
@@ -19,8 +35,25 @@ export const TenantMembers = () => {
             <FormattedMessage defaultMessage="View and manage organization members" id="Tenant Members / Subheader" />
           }
         />
-        <InvitationForm />
-        <TenantMembersList />
+        {isPersonal ? (
+          <Alert className="py-8" data-testid="tenant-members-alert">
+            <Paragraph className="pb-4">If you wish to invite people, you need to create an organization.</Paragraph>
+            <Button
+              data-testid="tenant-members-create-button"
+              color="primary"
+              size="sm"
+              onClick={handleLastInvitationClick}
+            >
+              <Plus className="mr-1" size="16" />{' '}
+              <FormattedMessage defaultMessage="Create new tenant" id="TenantSwitch / Create new tenant" />
+            </Button>
+          </Alert>
+        ) : (
+          <div data-testid="tenant-members-list">
+            <InvitationForm />
+            <TenantMembersList />
+          </div>
+        )}
       </div>
     </TabsContent>
   );
