34b6de2a0d6e15f898b334239a613f39e73c686e
Sebastian Drejkarz <sdrejkarz@apptension.com>
2024-05-17 12:04:36 +0200

    fix: refactor tests

--- packages/webapp-libs/webapp-tenants/src/components/tenantSwitch/__tests__/tenantSwitch.component.spec.tsx
+++ packages/webapp-libs/webapp-tenants/src/components/tenantSwitch/__tests__/tenantSwitch.component.spec.tsx
@@ -21,105 +21,95 @@ jest.mock('react-router-dom', () => {
   };
 });
 
+const personalTenantName = 'Test Tenant';
+const organizationTenantName = 'Organization Tenant';
+const organizationPendingTenantName = 'Organization Pending Tenant';
+
+const personalTenant = tenantFactory({
+  name: personalTenantName,
+  type: TenantType.PERSONAL,
+  membership: { role: TenantUserRole.OWNER },
+});
+const organizationTenant = tenantFactory({
+  name: organizationTenantName,
+  type: TenantType.ORGANIZATION,
+  membership: { role: TenantUserRole.MEMBER, invitationAccepted: true },
+});
+const organizationPendingTenant = tenantFactory({
+  name: organizationPendingTenantName,
+  type: TenantType.ORGANIZATION,
+  membership: { role: TenantUserRole.MEMBER, invitationAccepted: false },
+});
+
+const tenants = [personalTenant, organizationTenant, organizationPendingTenant];
+const getApolloMocks = () => [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
+
 describe('TenantSwitch: Component', () => {
   const Component = () => <TenantSwitch />;
 
   it('should render current tenant name', async () => {
-    const tenantName = 'Test Tenant';
-    const tenants = [
-      tenantFactory({ name: tenantName, membership: { role: TenantUserRole.OWNER } }),
-      tenantFactory({
-        name: 'Second name',
-        type: TenantType.ORGANIZATION,
-        membership: { role: TenantUserRole.MEMBER },
-      }),
-    ];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks });
-    expect(await screen.findByText(tenantName)).toBeInTheDocument();
+    render(<Component />, { apolloMocks: getApolloMocks() });
+
+    expect(await screen.findByText(personalTenantName)).toBeInTheDocument();
     expect(screen.getByTestId('tenant-settings-btn')).toBeInTheDocument();
   });
 
   it('should render correct tenant name when param in url', async () => {
-    const tenantName = 'Test Tenant';
-    const tenants = [
-      tenantFactory({ name: 'Personal name', membership: { role: TenantUserRole.OWNER } }),
-      tenantFactory({ name: tenantName, type: TenantType.ORGANIZATION, membership: { role: TenantUserRole.MEMBER } }),
-    ];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
     const routerProps = createMockRouterProps(RoutesConfig.home, { tenantId: tenants[1].id });
-    render(<Component />, { apolloMocks, routerProps, TenantWrapper });
-    expect(await screen.findByText(tenantName)).toBeInTheDocument();
+    render(<Component />, { apolloMocks: getApolloMocks(), routerProps, TenantWrapper });
+
+    expect(await screen.findByText(organizationTenantName)).toBeInTheDocument();
     expect(screen.queryByTestId('tenant-settings-btn')).not.toBeInTheDocument();
   });
 
-  it('should not render organisation or invitation if not tenants', async () => {
-    const tenantName = 'Test Tenant';
-    const tenants = [tenantFactory({ name: tenantName, membership: { role: TenantUserRole.OWNER } })];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks });
-    expect(await screen.findByText(tenantName)).toBeInTheDocument();
+  it('should not render organisation and invitation labels if only personal tenant', async () => {
+    render(<Component />, {
+      apolloMocks: [fillCommonQueryWithUser(currentUserFactory({ tenants: [personalTenant] }))],
+    });
+    expect(await screen.findByText(personalTenantName)).toBeInTheDocument();
+
+    const currentTenantButton = await screen.findByText(personalTenantName);
+    await userEvent.click(currentTenantButton);
+
     expect(screen.queryByText(/organizations/i)).not.toBeInTheDocument();
     expect(screen.queryByText(/invitations/i)).not.toBeInTheDocument();
+    expect(screen.queryByTestId('tenant-invitation-pending-btn')).not.toBeInTheDocument();
   });
 
   it('should handle invitation tenant click', async () => {
-    const currentTenantName = 'Personal name';
-    const tenantName = 'Test Tenant';
-    const tenants = [
-      tenantFactory({ name: currentTenantName, membership: { role: TenantUserRole.OWNER } }),
-      tenantFactory({
-        name: tenantName,
-        type: TenantType.ORGANIZATION,
-        membership: { role: TenantUserRole.MEMBER, invitationAccepted: false },
-      }),
-    ];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks, TenantWrapper });
+    render(<Component />, { apolloMocks: getApolloMocks(), TenantWrapper });
 
-    const currentTenantButton = await screen.findByText(currentTenantName);
+    const currentTenantButton = await screen.findByText(personalTenantName);
     await userEvent.click(currentTenantButton);
 
     expect(await screen.findByText(/invitations/i)).toBeInTheDocument();
-    const invitationTenantButton = await screen.findByText(tenantName);
+    const invitationTenantButton = await screen.findByText(organizationPendingTenantName);
 
     await userEvent.click(invitationTenantButton);
 
-    expect(mockNavigate).toHaveBeenCalledWith(`/en/tenant-invitation/${tenants[1].membership.invitationToken}`);
+    expect(mockNavigate).toHaveBeenCalledWith(
+      `/en/tenant-invitation/${organizationPendingTenant.membership.invitationToken}`
+    );
   });
 
   it('should handle tenant change', async () => {
-    const currentTenantName = 'Personal name';
-    const tenantName = 'Test Tenant';
-    const tenants = [
-      tenantFactory({ name: currentTenantName, membership: { role: TenantUserRole.OWNER } }),
-      tenantFactory({
-        name: tenantName,
-        type: TenantType.ORGANIZATION,
-        membership: { role: TenantUserRole.MEMBER, invitationAccepted: true },
-      }),
-    ];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks, TenantWrapper });
+    render(<Component />, { apolloMocks: getApolloMocks(), TenantWrapper });
 
-    const currentTenantButton = await screen.findByText(currentTenantName);
+    const currentTenantButton = await screen.findByText(personalTenantName);
     await userEvent.click(currentTenantButton);
 
     expect(await screen.findByText(/organizations/i)).toBeInTheDocument();
-    const invitationTenantButton = await screen.findByText(tenantName);
+    const invitationTenantButton = await screen.findByText(organizationTenantName);
 
     await userEvent.click(invitationTenantButton);
 
     expect(mockNavigate).toHaveBeenCalledWith(`/en/${tenants[1].id}`);
   });
 
-  it('should handle settings click', async () => {
-    const tenantName = 'Test Tenant';
-    const tenants = [tenantFactory({ name: tenantName, membership: { role: TenantUserRole.OWNER } })];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks });
+  it('should handle create new tenant click', async () => {
+    render(<Component />, { apolloMocks: getApolloMocks() });
 
-    const currentTenantButton = await screen.findByText(tenantName);
+    const currentTenantButton = await screen.findByText(personalTenantName);
     await userEvent.click(currentTenantButton);
 
     const newTenantButton = await screen.findByText(/create new tenant/i);
@@ -129,10 +119,7 @@ describe('TenantSwitch: Component', () => {
   });
 
   it('should handle settings click', async () => {
-    const tenantName = 'Test Tenant';
-    const tenants = [tenantFactory({ name: tenantName, membership: { role: TenantUserRole.OWNER } })];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks });
+    render(<Component />, { apolloMocks: getApolloMocks() });
 
     const settingsButton = await screen.findByTestId('tenant-settings-btn');
     await userEvent.click(settingsButton);
@@ -141,19 +128,13 @@ describe('TenantSwitch: Component', () => {
   });
 
   it('should handle invitation pending badge click', async () => {
-    const tenants = [
-      tenantFactory({ membership: { role: TenantUserRole.OWNER } }),
-      tenantFactory({
-        type: TenantType.ORGANIZATION,
-        membership: { role: TenantUserRole.MEMBER, invitationAccepted: false },
-      }),
-    ];
-    const apolloMocks = [fillCommonQueryWithUser(currentUserFactory({ tenants }))];
-    render(<Component />, { apolloMocks });
+    render(<Component />, { apolloMocks: getApolloMocks() });
 
     const invitationPendingButton = await screen.findByTestId('tenant-invitation-pending-btn');
     await userEvent.click(invitationPendingButton);
 
-    expect(mockNavigate).toHaveBeenCalledWith(`/en/tenant-invitation/${tenants[1].membership.invitationToken}`);
+    expect(mockNavigate).toHaveBeenCalledWith(
+      `/en/tenant-invitation/${organizationPendingTenant.membership.invitationToken}`
+    );
   });
 });
