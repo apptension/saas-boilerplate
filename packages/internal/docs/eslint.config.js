c71cb6722299ae865add63fc7528c890272e456c
Emil Litwiniec <elitwiniec@apptension.com>
2024-05-23 14:25:01 +0200

    deps: Config rewrite docs

---
+++ packages/internal/docs/eslint.config.js
@@ -0,0 +1,27 @@
+const rootConfig = require('../../../eslint.config.js');
+
+module.exports = [
+  ...rootConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      'import/no-unresolved': [2, { ignore: ['^@docusaurus', '^@theme'] }],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  {
+    ignores: [
+      '**/node_modules/**/*',
+      '**/build/**/*',
+      '**/.docusaurus/**/*',
+      '**/cdk.out/**/*',
+    ],
+  },
+];
