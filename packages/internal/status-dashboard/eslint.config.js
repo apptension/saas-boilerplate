8bd3ed7665d63a0fc0f34b9773b1f7601683a3c6
Emil Litwiniec <elitwiniec@apptension.com>
2024-05-23 14:22:52 +0200

    deps: Config rewrite status-dasboard

---
+++ packages/internal/status-dashboard/eslint.config.js
@@ -0,0 +1,18 @@
+const baseConfig = require('../../../eslint.config.js');
+
+module.exports = [
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  { ignores: ['node_modules/**/*', 'build/**/*', 'cdk.out/**/*'] },
+];
