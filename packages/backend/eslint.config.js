eb3ac22a722a312edde729023c7e5de266d9dcfd
Emil Litwiniec <elitwiniec@apptension.com>
2024-05-24 11:42:32 +0200

    deps: Config rewrite backend

---
+++ packages/backend/eslint.config.js
@@ -0,0 +1,23 @@
+const rootConfig = require('../../eslint.config.js');
+
+module.exports = [
+  ...rootConfig,
+  {
+    ignores: [
+      '!**/*',
+      '**/node_modules/**/*',
+      '**/cdk.out/**/*',
+      '**/__pypackages__/**/*',
+      '**/apps/finances/static/djstripe/jquery.modal.min.js',
+    ],
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+];
