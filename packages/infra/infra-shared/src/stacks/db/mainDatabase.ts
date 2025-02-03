029325c17bb3fdc6575c13587274dc5eb465e8d0
mkleszcz <mkleszcz@users.noreply.github.com>
2025-02-03 08:49:10 +0100

    fix: Update outdated RDS version, update CDK dependency (#631)

--- packages/infra/infra-shared/src/stacks/db/mainDatabase.ts
+++ packages/infra/infra-shared/src/stacks/db/mainDatabase.ts
@@ -68,7 +68,7 @@ export class MainDatabase extends Construct {
       vpc: props.vpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
       engine: rds.DatabaseInstanceEngine.postgres({
-        version: rds.PostgresEngineVersion.VER_16_1,
+        version: rds.PostgresEngineVersion.VER_17_2,
       }),
       instanceType: ec2.InstanceType.of(
         ec2.InstanceClass.T3,
